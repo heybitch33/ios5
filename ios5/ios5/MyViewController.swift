42ed3c1a50af4710f6b67c18c44d5e46716a5338
Ivan Yavtushenko <ivanko3003@gmail.com>
2021-12-05 14:34:25 +0200

    add views to VC

--- ios5/ios5/MyViewController.swift
+++ ios5/ios5/MyViewController.swift
@@ -8,26 +8,48 @@
 import UIKit
 
 final class MyViewController: UIViewController {
-
-    let someLabel = UILabel()
-    
-    override func viewDidLoad() {
+  
+  let someLabel = UILabel()
+  let buttonView = ButtonView()
+  let textView = TextView()
+  
+  override func viewDidLoad() {
     super.viewDidLoad()
     setupSubviews()
   }
-
+  
 }
 
 private extension MyViewController {
+  
+  func setupSubviews() {
+    view.backgroundColor = .white
     
-    func setupSubviews() {
-        view.backgroundColor = .white
-        someLabel.text = "This is VC Label"
-        someLabel.textAlignment = .center
-        someLabel.frame = CGRect(x: UIScreen.main.bounds.width / 2 - 100,
-                                 y: 20,
-                                 width: 200,
-                                 height: 25)
-        view.addSubview(someLabel)
-    }
+    let width: CGFloat =  200
+    let height: CGFloat =  25
+    
+    someLabel.text = "This is VC Label"
+    someLabel.textAlignment = .center
+    someLabel.frame = CGRect(
+      x: UIScreen.main.bounds.width / 2 - 100,
+      y: 20,
+      width: width,
+      height: height)
+    
+    buttonView.frame = CGRect(
+      x: someLabel.frame.origin.x,
+      y: someLabel.frame.origin.y + height + 10,
+      width: width,
+      height: height)
+    
+    textView.frame = CGRect(
+      x: buttonView.frame.origin.x,
+      y: buttonView.frame.origin.y + height + 10,
+      width: width,
+      height: height)
+    
+    view.addSubview(someLabel)
+    view.addSubview(buttonView)
+    view.addSubview(textView)
+  }
 }
