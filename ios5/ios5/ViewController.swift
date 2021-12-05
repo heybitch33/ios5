3259a459681bc549e3f1ff6bc4819c25cfe0a292
Ivan Yavtushenko <ivanko3003@gmail.com>
2021-12-05 11:13:15 +0200

    create project

---
+++ ios5/ios5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios5
+//
+//  Created by Ivan Yavtushenko on 05.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
